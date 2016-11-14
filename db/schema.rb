86f3d331f4c61691c847ecd5c6a452a2a6ff109f
Austin Berard <austinberard@outlook.com>
2016-11-14 15:13:45 -0500

    Changed header links to match other ones, and added help page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114184938) do
+ActiveRecord::Schema.define(version: 20161114193616) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20161114184938) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.text     "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
