c4a4600ee19a0160aeeb0d1094dd5f45105d976d
Devin Gould <devin.gould@voltserver.com>
2016-12-12 15:46:10 -0500

    Various bug fixes

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,6 @@
 module ProjectsHelper
+  def followers_list project
+    list = @project.followers.reject {|follower| follower.id == @project.owner.id}.map {|follower| follower.email}.join(",")
+    return "No followers" if list.empty?
+  end
 end
